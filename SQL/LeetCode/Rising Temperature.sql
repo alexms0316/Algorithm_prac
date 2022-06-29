793192a0d6136b013d6cc6032406db0ed477ad20
MinseokChoi <alexms0316@gmail.com>
2022-06-29 15:32:59 +0900

    Rising Temperature

---
+++ SQL/LeetCode/Rising Temperature.sql
@@ -0,0 +1,4 @@
+SELECT today.Id
+FROM Weather AS today
+    INNER JOIN Weather AS yesterday ON DATE_ADD(yesterday.recordDate, INTERVAL 1 DAY) = today.recordDate
+WHERE today.temperature > yesterday.temperature
