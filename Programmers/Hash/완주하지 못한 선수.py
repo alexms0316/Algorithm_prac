5e378a9f80c90fbf4bf356b19d8470b0b719ce1d
MinseokChoi <alexms0316@gmail.com>
2022-07-13 12:08:03 +0900

    [Fix] 완주하지 못한 선수

--- Programmers/Hash/완주하지 못한 선수.py
+++ Programmers/Hash/완주하지 못한 선수.py
@@ -1,4 +1,14 @@
+# 1st try (효율성 검사에서 실패)
 def solution(participant, completion):
     for i in completion:
         participant.remove(i)
     return participant[0]
+
+#2nd try 
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for p,c in zip(participant, completion):
+        if p != c:
+            return p
+    return participant.pop()
